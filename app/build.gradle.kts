154173c03883b06d7a6981b8783f713acbb9727e
jadg13 <jose.duran@uamv.edu.ni>
2024-06-10 19:05:51 -0600

    Second commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
 }
---
+++ app/src/main/java/com/uam/agenda/remote/ApiAdapter.kt
@@ -0,0 +1,18 @@
+package com.uam.agenda.remote
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiAdapter {
+    val BASE_URL = "http://localhost/apiAgenda"
+
+    val okHttpClient = OkHttpClient.Builder().build()
+    fun getInstance() : Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+}
